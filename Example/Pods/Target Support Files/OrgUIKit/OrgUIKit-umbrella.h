83721c173e484bd0f474bb3f556e1b3069259d05
limingchen <mingchen.li@dianping.com>
2017-03-02 11:21:42 +0800

    add OrgUIKit.h

--- Example/Pods/Target Support Files/OrgUIKit/OrgUIKit-umbrella.h
+++ Example/Pods/Target Support Files/OrgUIKit/OrgUIKit-umbrella.h
@@ -2,6 +2,9 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "UIColor+util.h"
+#import "UIDevice+IdentifierAddition.h"
+#import "UIView+layout.h"
 
 FOUNDATION_EXPORT double OrgUIKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char OrgUIKitVersionString[];
---
+++ OrgUIKit/Classes/OrgUIKit.h
@@ -0,0 +1,16 @@
+//
+//  OrgUIKit.h
+//  Pods
+//
+//  Created by lmc on 2017/3/2.
+//
+//
+
+#ifndef OrgUIKit_h
+#define OrgUIKit_h
+
+#import "UIColor+util.h"
+#import "UIDevice+IdentifierAddition.h"
+#import "UIView+layout.h"
+
+#endif /* OrgUIKit_h */
